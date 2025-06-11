49e218ac32d1baad8a4af5b0490c7369a82e8cdc
bhuvana816 <bhuvanass2005@gmail.com>
2025-06-11 10:44:01 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -27,7 +27,7 @@ const projects: Project[] = [
     description: "The Nykaa Clone is a fully functional e-commerce web application designed to offer a seamless shopping experience for beauty, skincare, and wellness products. It includes essential features such as user authentication, product browsing with filters, cart and wishlist management, and secure checkout. An integrated admin dashboard allows for easy management of products, categories, and orders. The platform is responsive across devices, ensuring a smooth user experience on both desktop and mobile.",
     image: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     github: "https://github.com/bhuvana816/nykaa",
-    live: "https://https://portfolio2-f57f.vercel.app/",
+    live: "https://portfolio2-f57f.vercel.app/",
     tags: ["HTML", "Tailwind CSS", "Framer Motion"]
    }
 ];
