b36d4f19d865ffc7a06e8428be790a291245d428
bhuvana816 <bhuvanass2005@gmail.com>
2025-07-08 11:02:53 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-""import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import AnimatedSection from './AnimatedSection';
 import emailjs from '@emailjs/browser';
